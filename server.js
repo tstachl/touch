c6233ae01d14c71e9096a449447ba360509445bc
Thomas Stachl <thomas@stachl.me>
2012-05-18 15:53:43 +0100

    debugging server.js

--- server.js
+++ server.js
@@ -10,17 +10,21 @@ var express    = require('express'),
     env        = process.env.NODE_ENV || 'development',
     publicPath = __dirname + '/public',
     index      = publicPath + '/index.html',
-    address, serverHostname, serverPort, serverLocation, makeRequest, isEmptyObject, serialize,
+    address, serverHostname, serverPort, serverLocation, makeRequest, isEmptyObject, serialize;
 
 // Create Server
-    server     = express.createServer({
-      ca:   env == 'development' ? fs.readFileSync('certs/sub.class1.server.ca.pem') : null,
-      key:  env == 'development' ? fs.readFileSync('certs/ssl.key') : null,
-      cert: env == 'development' ? fs.readFileSync('certs/ssl.crt') : null
+if (env === 'development')
+  var server   = express.createServer({
+      ca:   fs.readFileSync('certs/sub.class1.server.ca.pem'),
+      key:  fs.readFileSync('certs/ssl.key'),
+      cert: fs.readFileSync('certs/ssl.crt')
     });
+else
+  var server   = express.createServer();
 
 // Configure
 server.configure(function() {
+  server.use(express.logger());
   // Standard
   server.use(express.errorHandler());
   server.use(express.bodyParser());
@@ -29,55 +33,55 @@ server.configure(function() {
   // Routing
   server.use(server.router);
   server.use(express.static(publicPath));
-  server.use(express.directory(publicPath));
 });
 
-isEmptyObject = function( obj ) {
-  for ( var name in obj ) {
-    return false;
-  }
-  return true;
-};
+// isEmptyObject = function( obj ) {
+//   for ( var name in obj ) {
+//     return false;
+//   }
+//   return true;
+// };
+// 
+// serialize = function(obj) {
+//   var str = [];
+//   for(var p in obj)
+//      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+//   return str.join("&");
+// };
+// 
+// makeRequest = function(req, rsp) {
+//   var options = {}; 
+//   options.headers = {};
+//   for (var key in req.headers) {
+//     if (key.indexOf('accept-') === 0) continue;
+//     options.headers[key.charAt(0).toUpperCase() + key.substr(1)] = req.headers[key];
+//   }
+// 
+//   options.url = req.headers.proxy;
+//   options.method = req.method;
+//   
+//   if (!isEmptyObject(req.body)) {
+//     if (req.headers['content-type'].indexOf('x-www-form') !== -1) {
+//       options.body = serialize(req.body);
+//     } else {
+//       options.body = JSON.stringify(req.body);
+//     }
+//   }
+//   
+//   request(options, function(e, r, body) {
+//     if (body) {
+//       rsp.send(body, r.headers, r.statusCode);
+//     } else {
+//       rsp.send(r.headers, r.statusCode);
+//     }
+//   });
+// };
 
-serialize = function(obj) {
-  var str = [];
-  for(var p in obj)
-     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-  return str.join("&");
-};
-
-makeRequest = function(req, rsp) {
-  var options = {}; 
-  options.headers = {};
-  for (var key in req.headers) {
-    if (key.indexOf('accept-') === 0) continue;
-    options.headers[key.charAt(0).toUpperCase() + key.substr(1)] = req.headers[key];
-  }
-
-  options.url = req.headers.proxy;
-  options.method = req.method;
-  
-  if (!isEmptyObject(req.body)) {
-    if (req.headers['content-type'].indexOf('x-www-form') !== -1) {
-      options.body = serialize(req.body);
-    } else {
-      options.body = JSON.stringify(req.body);
-    }
-  }
-  
-  request(options, function(e, r, body) {
-    if (body) {
-      rsp.send(body, r.headers, r.statusCode);
-    } else {
-      rsp.send(r.headers, r.statusCode);
-    }
-  });
-};
-
-server.all('/api', makeRequest);
+//server.all('/api', makeRequest);
 server.get('/', function(req, res) {
-  res.contentType(index);
-  res.sendfile(index);
+  res.send('Hello World!');
+  // res.contentType(index);
+  // res.sendfile(index);
 });
 
 // Listen
